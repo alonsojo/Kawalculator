544c256af03262275868d6f68d5bd5fde41b1ba9
Jose Luis Alonso <chelizjose@gmail.com>
2018-10-30 23:09:27 -0700

    Version 0.3
    +Added tokenizer
    
    Bugs:
    -Doesnt tokenize negative numbers properly

--- Kawalculator/Kawalculator/kawalculator.cpp
+++ Kawalculator/Kawalculator/kawalculator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
+
 
 using namespace std;
 
@@ -44,8 +46,40 @@ int main(int argc, char *argv[])
 			validInput = validateInput(input);
 		}
 
+		//Prints validated input
 		cout << '\t' << input << " = " <<endl <<endl;
+
+		//Tokenizes input based on allowed characters
 		
+		vector<string> wordVector;
+		int len = 0, i=0,j = 0;
+		for (i = 0; i < input.length(); i++)
+		{
+
+			if (isdigit(input[i]) || input[i]=='.')
+			{
+				j = i;
+				while (isdigit(input[j]) || input[j] == '.')
+				{
+					len++;
+					j++;
+				}
+				wordVector.push_back(input.substr(i, len));
+				i = j - 1;
+				len = 0;
+			}
+			else 
+			{
+				wordVector.push_back(input.substr(i, 1));
+				
+			}
+		}
+		//Tokens
+		for (int i = 0; i<wordVector.size(); i++)
+			cout << wordVector[i] << "\n";
+			
+		cout << endl;
+		//Menu part 2
 		while (true) {
 			cout << "\tDo you want to enter a new expression [y/n]: ";  
 			code = cin.get();
@@ -75,6 +109,16 @@ bool isValidSPChar(char letter)
 			valid = true;
 	return valid;
 }
+/*
+int nextTokenDelimiter(const string &input, int startPoint)
+{
+	int valid = false;
+
+	for (int i = startPoint; i < input.length(); i++)
+		if (letter == allowedSPSymbols[i])
+			valid = true;
+	return valid;
+}*/
 
 bool validateInput(string input)
 {
@@ -83,6 +127,7 @@ bool validateInput(string input)
 
 	for (int i = 0; i < input.length(); i++)
 	{
+	
 		//Not a digit
 		if (!isdigit(input[i]))
 		{
@@ -92,16 +137,30 @@ bool validateInput(string input)
 			//First character can only be an [(], a digit or [-] 
 			if (i == 0)
 			{
+				//Not a digit, but a (
+				if (input[i] == allowedSPSymbols[0] && input[i + 1] == allowedSPSymbols[1])
+					return false;
 				//Negative sign
 				if (input[i] == allowedSPSymbols[6])
 					if (!isdigit(input[i + 1]))
 						return false;
+				//No operators can be at the beginning of the expression
 				for (int j = 1; j < allowedSPSymbols.length(); j++)
 				{
 					if (input[0] == allowedSPSymbols[j] && j != 6)
 						return false;
 				}
 			}
+			//Last character can only be an [)] or a digit
+			if (i == input.length() - 1)
+			{
+				for (int j = 0; j < allowedSPSymbols.length(); j++)
+				{
+					if (j != 1 && input[i] == allowedSPSymbols[j])
+						return false;
+				}
+			}
+
 			
 			//Not a digit, but a (
 			if (input[i] == allowedSPSymbols[0])
@@ -142,7 +201,7 @@ bool validateInput(string input)
 			//[.] can only have digits on both sides
 			if (input[i] == allowedSPSymbols[7])
 			{
-				if (!isdigit(input[i + 1]) || !isdigit(input[i - 1]))
+				if (!isdigit(input[i - 1]) || !isdigit(input[i + 1]))
 					return false;
 			}
 		}
